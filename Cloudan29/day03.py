4ad5f49b7034859adbc1e8cdc3fb34fea7c25fcc
Dan Cloutier <guitarman.dan29@gmail.com>
2019-12-03 01:41:19 -0500

    submit day3 and prep day4

---
+++ Cloudan29/day03.py
@@ -0,0 +1,31 @@
+A,B = open("Cloudan29/inputs/day03.txt").read().split("\n")
+A,B = [instructions.split(",") for instructions in [A,B]]
+dirs = {"R": (1,0), "L": (-1,0), "U": (0,1), "D": (0,-1)}
+
+def points(arr):
+    x = y = 0
+    ans = {}
+    length = 0
+    for ins in arr:
+        dir, amnt = ins[0], int(ins[1:])
+        for _ in range(amnt):
+            length += 1
+            x += dirs[dir][0]
+            y += dirs[dir][1]
+            if (x,y) not in ans:
+                ans[(x,y)] = length
+
+    return ans
+
+PA = points(A)
+PB = points(B)
+crosses = PA.keys()&PB.keys()
+
+def part1():
+    return min([abs(x) + abs(y) for (x,y) in crosses])
+
+def part2():
+    return min([PA[cross] + PB[cross] for cross in crosses])
+
+print (part1())
+print (part2())
---
+++ Cloudan29/day04.py
@@ -0,0 +1,10 @@
+inp = open("Cloudan29/inputs/day04.txt")
+
+def part1():
+    pass
+
+def part2():
+    pass
+
+print (part1())
+# print (part2())
