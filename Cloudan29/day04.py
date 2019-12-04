88672e6733eedc2e054bd516a7862d5291f5421b
Dan Cloutier <guitarman.dan29@gmail.com>
2019-12-04 01:06:28 -0500

    submission for day 4 and prep for day 5

--- Cloudan29/day04.py
+++ Cloudan29/day04.py
@@ -1,10 +1,33 @@
 inp = open("Cloudan29/inputs/day04.txt")
+low, high = [int(num) for num in inp.read().split("-")]
 
 def part1():
-    pass
+    ans = 0
+    for i in range(low, high):
+        password = [int(j) for j in str(i)]
+        if password != sorted(password):
+            continue
+
+        for digit in password:
+            if password.count(digit) >= 2:
+                ans += 1
+                break
+
+    return ans
 
 def part2():
-    pass
+    ans = 0
+    for i in range(low, high):
+        password = [int(j) for j in str(i)]
+        if password != sorted(password):
+            continue
+
+        for digit in password:
+            if password.count(digit) == 2:
+                ans += 1
+                break
+
+    return ans
 
 print (part1())
-# print (part2())
+print (part2())
---
+++ Cloudan29/day05.py
@@ -0,0 +1,11 @@
+inp = open("Cloudan29/inputs/day05.txt")
+
+def part1():
+    pass
+
+def part2():
+    pass
+
+
+print (part1())
+# print (part2())
