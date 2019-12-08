d1608136f1f6c380661545e2e8f837e9e7be872a
Dan Cloutier <guitarman.dan29@gmail.com>
2019-12-08 00:54:59 -0500

    submission for day 8, prep day 9

--- day08.py
+++ day08.py
@@ -1,10 +1,39 @@
-inp = open("inputs/day08.txt")
+H = 6
+W = 25
+IMAGE_SIZE = H * W
+inp = open("inputs/day08.txt").read()
+layers = []
+while inp:
+    layers.append(inp[:IMAGE_SIZE])
+    inp = inp[IMAGE_SIZE:]
+        
 
 def part1():
-    pass
+    row_with_lowest_0s = []
+    lowest_num_of_zeros = IMAGE_SIZE
+    for layer in layers:
+        if layer.count('0') < lowest_num_of_zeros:
+            lowest_num_of_zeros = layer.count('0')
+            row_with_lowest_0s = layer
+        
+    return row_with_lowest_0s.count('1') * row_with_lowest_0s.count('2')
+
 
 def part2():
-    pass
+    decoded = list(layers[0])
+    for layer in layers:
+        for i in range(len(layer)):
+            if decoded[i] == '2':
+                decoded[i] = layer[i]
+
+    image = ''
+    while decoded:
+        for i in range(W):
+            image += decoded[i]
+        image += '\n'
+        decoded = decoded[W:]
+
+    return image.replace('0', ' ')
 
 
 print (part1())
---
+++ day09.py
@@ -0,0 +1,10 @@
+inp = open("inputs/day09.txt").read()
+
+def part1():
+    pass
+
+def part2():
+    pass
+
+print (part1())
+print (part2())
