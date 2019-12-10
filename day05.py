a2229c329902d5081fb9a91815d8ca0ddf469619
Dan Cloutier <guitarman.dan29@gmail.com>
2019-12-10 16:47:18 -0500

    small adjustment to day 5

--- day05.py
+++ day05.py
@@ -4,24 +4,23 @@ inp = open("inputs/day05.txt").read().split(",")
 def part1():
     machine = Machine([int(instr) for instr in inp])
     interrupt = False
-    while not interrupt:
+    while True:
         try:
             machine.run_machine([1])
         except Output as e:
             print (e.output)
         except Interrupt:
-            interrupt = True
+            break
 
 def part2():
     machine = Machine([int(instr) for instr in inp])
-    interrupt = False
-    while not interrupt:
+    while True:
         try:
             machine.run_machine([5])
         except Output as e:
             print (e.output)
         except Interrupt:
-            interrupt = True
+            break
 
 part1()
 part2()
